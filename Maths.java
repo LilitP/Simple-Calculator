e22a9f30649fe351958d6661598531c3774a9b07
LilitP <pahlevanyanlilit@gmail.com>
2018-02-12 12:55:47 +0400

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,29 @@
+import java.lang.Math;
+
+public class Maths {
+
+	double add(double a, double b) {
+		double result = a + b;
+		return result;
+	}
+	double subtract(double a, double b) {
+		double result = a - b;
+		return result;
+	}
+	double multiply(double a, double b) {
+		double result = a * b;
+		return result;
+	}
+	double divide(double a, double b) {
+		double result = a / b;
+		return result;
+	}
+	double powm(double a, double b) {
+		double result = Math.pow(a, b);
+		return result;
+	}
+	double sqroot(double a) {
+		double result = Math.sqrt(a);
+		return result;
+	}
+}
